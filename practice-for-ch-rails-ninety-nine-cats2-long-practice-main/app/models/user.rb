38c982a5e7412fb329e5251268601699e006be3c
Evan Ryan <evan.hundred@gmail.com>
2022-10-24 14:57:47 -0400

    create users controller

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -14,10 +14,6 @@ class User < AplicationRecord
         end
     end
 
-    def ensure_session_token
-        self.session_token ||= SecureRandom::urlsafe_base64
-    end
-
     def password=(password)
         self.password_digest = BCrypt::Password.create(password)
         @password = password
@@ -26,6 +22,22 @@ class User < AplicationRecord
     def is_password?(password)
         password_object = BCrypt::Password.new(self.password_digest)
         password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
     end
 
 end
